cb0716829216331ac05a4ce1b9042c926789a6c1
Michał Łazowik <m.lazowik@student.uw.edu.pl>
2017-09-04 23:13:43 +0200

    Add init events

--- fs/proc/procmon.c
+++ fs/proc/procmon.c
@@ -44,48 +44,102 @@ static struct event_stream *create_stream(void)
 	return stream;
 }
 
-void add_event(struct task_struct *task, uint32_t type)
+void fill_procmon_event(struct procmon_event *p_event, uint32_t type,
+	struct task_struct *task)
 {
-	struct event *event, *empty;
-	struct event_stream *stream;
-	struct list_head *pos;
-	struct procmon_event p_event;
 	const struct cred *cred;
 
 	cred = get_task_cred(task);
-	
-	p_event.type = type;
-	p_event.tid = task->pid;
-	p_event.pid = task->pid;
-	p_event.ppid = task_ppid_nr(task);
-	p_event.uid = cred->uid.val;
-	p_event.euid = cred->euid.val;
-	p_event.suid = cred->suid.val;
-	p_event.fsuid = cred->fsuid.val;
-	p_event.status = 42 << 8;
-	strcpy(p_event.comm, task->comm);
-	
+
+        p_event->type = type;
+        p_event->tid = task->pid;
+        p_event->pid = task->pid;
+        p_event->ppid = task_ppid_nr(task);
+        p_event->uid = cred->uid.val;
+        p_event->euid = cred->euid.val;
+        p_event->suid = cred->suid.val;
+        p_event->fsuid = cred->fsuid.val;
+        p_event->status = 42 << 8;
+        strcpy(p_event->comm, task->comm);
+
+	return;
+}
+
+/**
+ * Appends p_event at the end of stream.
+ *
+ * *p_event is copied.
+ */
+static int append_event(struct event_stream *stream, struct procmon_event *p_event) 
+{
+	struct event *event, *empty;
+
+	empty = create_empty_event();
+	if (!empty) return -ENOMEM;
+	list_add_tail(&empty->list, &stream->events);
+	event = list_entry(empty->list.prev, struct event, list);
+	event->p_event = *p_event;
+	complete_all(&event->happened);
+
+	return 0;
+}
+
+/**
+ * Adds event from given task of given type to all open event streams.
+ */
+void add_event(struct task_struct *task, uint32_t type)
+{
+	struct event_stream *stream;
+	struct list_head *pos;
+	struct procmon_event p_event;
+
+	fill_procmon_event(&p_event, type, task);
+
 	list_for_each(pos, &event_streams) {
 		stream = list_entry(pos, struct event_stream, list);
-		empty = create_empty_event();
-		list_add_tail(&empty->list, &stream->events);
-		event = list_entry(empty->list.prev, struct event, list);
-		event->p_event = p_event;
-		complete_all(&event->happened);
+		append_event(stream, &p_event);
 	}
+}
+
+/**
+ * Adds event of type PROCMON_EVENT_EXISTING from given task and its children
+ * to the end of the given event stream.
+ *
+ * Ignores init process.
+ */
+static int add_init_events(struct event_stream *stream, struct task_struct *task)
+{
+	struct task_struct *child;
+	struct list_head *list;
+	struct procmon_event p_event;
+	int ret;
+
+	if (task->pid != 0) {
+		fill_procmon_event(&p_event, PROCMON_EVENT_EXISTING, task);
+	
+		ret = append_event(stream, &p_event);
+		if (ret != 0) return ret;
+	}
+	
+	list_for_each(list, &task->children) {
+		child = list_entry(list, struct task_struct, sibling);
+		ret = add_init_events(stream, child);
+		if (ret != 0) return ret;
+	}
+
+	return 0;
 }
 
 static int procmon_open(struct inode *inode, struct file *file)
 {
 	struct event_stream *stream;
 	struct event *empty;
+	int ret;
 
 	printk("procmon: open");
 
 	stream = create_stream();
-	if (!stream) {
-		return -ENOMEM;
-	}
+	if (!stream) return -ENOMEM;
 	
 	file->private_data = stream;
 	
@@ -93,6 +147,8 @@ static int procmon_open(struct inode *inode, struct file *file)
 	if (!empty) return -ENOMEM;
 	list_add_tail(&empty->list, &stream->events);
 
+	ret = add_init_events(stream, &init_task);
+	
 	return 0;
 }
 
